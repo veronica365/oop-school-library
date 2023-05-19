7296389fe10dde82395db6c388bbf7ebdce83dd0
veronica365 <naruth10@gmail.com>
2023-05-20 02:57:44 +0300

    Add person class with all the requirements

---
+++ person.rb
@@ -0,0 +1,20 @@
+class Person
+  attr_accessor :name, :age
+  attr_reader :id, :parent_permission
+
+  def initialize(age, name: 'Unkown', parent_permission: true)
+    @id = nil
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def of_age?
+    @age >= 18
+  end
+  private :of_age?
+end

74ddb195d4583d3efdbd187c66f0b8c27b940ee3
veronica365 <naruth10@gmail.com>
2023-05-20 21:23:09 +0300

    add functionality to create a rental

--- app.rb
+++ app.rb
@@ -1,52 +1,74 @@
-require_relative "person"
-require_relative "student"
-require_relative "teacher"
+require_relative 'person'
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'book'
 
 class App
   def initialize
     @books = []
     @people = []
     @students = []
+    @rentals = []
   end
 
-  def list_books
-    puts "\nListing all books"
-    @books.each do |book|
-      puts "[#{book.class}] Title: #{book.title}, Author: #{book.author}"
+  def list_books(message = 'All books')
+    return print "\nWe have no books now\n" if @books.empty?
+
+    puts "#{message unless message == 'All books'}\n"
+
+    @books.each_with_index do |book, index|
+      puts "#{index}) [#{book.class}] Title: #{book.title}, Author: #{book.author}"
     end
     puts "\n"
   end
 
-  def list_people
-    puts "\nListing all people"
-    @people.each do |person|
-      puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+  def list_people(message = 'All people')
+    return print "\nWe have no users now\n" if @people.empty?
+
+    puts "#{message unless message == 'All people'}\n"
+
+    @people.each_with_index do |person, index|
+      puts "#{index} [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
     puts "\n"
   end
 
   def list_rentals
+    puts "\nAll rentals"
+    return puts 'We have no rentals now' if @rentals.empty?
+
+    print 'ID of the person: '
+    id = gets.chomp.to_i
+
+    puts "Rentals: by #{id}\n"
+    @rentals.each_with_index do |rental, index|
+      if rental.person.id == id
+        puts "#{index} Date: #{rental.date}, Book: '#{rental.book.title}' by #{rental.book.author}"
+      else
+        puts "#{id} has no current rentals"
+      end
+    end
   end
 
   def create_user(choice)
-    print "Age: "
+    print 'Age: '
     age = gets.chomp.to_i
 
-    print "Name: "
+    print 'Name: '
     name = gets.chomp
 
-    optional_param = ""
-    if (choice == 1)
-      print "Has parent permission? [Y/N]: "
+    optional_param = ''
+    if choice == 1
+      print 'Has parent permission? [Y/N]: '
       optional_param = gets.chomp
-      optional_param = optional_param.upcase == "Y" ? true : false
+      optional_param = optional_param.upcase == 'Y'
     end
 
-    if (choice == 2)
-      print "Specialization: "
+    if choice == 2
+      print 'Specialization: '
       optional_param = gets.chomp
     end
-    return age, name, optional_param
+    [age, name, optional_param]
   end
 
   def create_student
@@ -62,20 +84,20 @@ class App
   end
 
   def create_person
-    print "Do you want to create a student (1) or a teacher (2)? [Input the number]: "
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     choice = gets.chomp
-    return create_student if choice == "1"
-    return create_teacher if choice == "2"
+    return create_student if choice == '1'
+    return create_teacher if choice == '2'
 
     puts "Invalid choice.\n\n"
   end
 
   def create_book
     puts "\nCreating a book"
-    print "Title: "
+    print 'Title: '
     title = gets.chomp
 
-    print "Author: "
+    print 'Author: '
     author = gets.chomp
 
     @books << Book.new(title, author)
@@ -83,5 +105,25 @@ class App
   end
 
   def create_rental
+    return puts "\nWe have no users now\n" if @people.empty?
+    return puts "\nWe have no books now\n" if @books.empty?
+
+    list_books('Select a book from the following list by number')
+    book_id = gets.chomp.to_i
+
+    renters_allowed = @people.select(&:can_use_services?)
+    return puts "\nWe have no allowed now\n" if renters_allowed.empty?
+
+    puts 'Select a person from the following list by number (not id)'
+    renters_allowed.each_with_index do |person, index|
+      puts "#{index} [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+    renter_person_id = gets.chomp.to_i
+
+    print 'Date: '
+    date = gets.chomp.to_s
+
+    @rentals << Rental.new(date, renters_allowed[renter_person_id], @books[book_id])
+    puts "Rental created successfully\n"
   end
 end

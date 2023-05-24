ac6129d79ed6762b83f96cbf09f1cc9b333cc448
veronica365 <naruth10@gmail.com>
2023-05-24 12:17:26 +0300

    fix fixable linter errors

--- files.rb
+++ files.rb
@@ -1,4 +1,4 @@
-require "json"
+require 'json'
 
 class Storage
   def open_or_create_file(filename)
@@ -6,12 +6,12 @@ class Storage
   end
 
   def read_file_content(filename)
-    return [] if !File.exist?(filename)
-    file = File.open(filename, "r")
+    return [] unless File.exist?(filename)
+
+    file = File.open(filename, 'r')
     file_data = file.read
-    if file_data.empty?
-      return []
-    end
+    return [] if file_data.empty?
+
     file_content = JSON.parse(file_data)
     file.close
     file_content
@@ -28,72 +28,72 @@ class Storage
     new_people = []
     people.each do |person|
       new_person = {
-        "age" => person.age,
-        "name" => person.name,
+        'age' => person.age,
+        'name' => person.name
       }
-      if person.class.to_s == "Student"
-        new_person["parent_permission"] = person.parent_permission
+      if person.instance_of?(::Student)
+        new_person['parent_permission'] = person.parent_permission
       else
-        new_person["specialization"] = person.specialization
+        new_person['specialization'] = person.specialization
       end
       new_people << new_person
     end
-    save_file_content("people.json", new_people)
+    save_file_content('people.json', new_people)
   end
 
   def save_books(books)
     new_books = []
     books.each do |book|
       new_book = {
-        "title" => book.title,
-        "author" => book.author,
+        'title' => book.title,
+        'author' => book.author
       }
       new_books << new_book
     end
-    save_file_content("books.json", new_books)
+    save_file_content('books.json', new_books)
   end
 
   def save_rentals(rentals)
     new_rentals = []
-    rentals.each_with_index do |rental, index|
+    rentals.each do |rental|
       new_rental = {
-        "person" => rental.person.name,
-        "book" => rental.book.title,
+        'person' => rental.person.name,
+        'book' => rental.book.title
       }
       new_rentals << new_rental
     end
-    save_file_content("rentals.json", new_rentals)
+    save_file_content('rentals.json', new_rentals)
   end
 
   def load_people
     people = []
-    people_data = read_file_content("people.json")
-    people_data.each do | person |
-      if person.has_key?("specialization")
-        people << Teacher.new(person["age"], person["name"], person["specialization"])
-      else
-        people << Student.new(person["age"], person["name"], person["parent_permission"])
+    people_data = read_file_content('people.json')
+    people_data.each do |person|
+      if person.key?('specialization')
+        return people << Teacher.new(person['age'], person['name'], person['specialization'])
       end
+
+      people << Student.new(person['age'], person['name'], person['parent_permission'])
     end
     people
   end
 
   def load_books
     books = []
-    book_data = read_file_content("books.json")
-    book_data.each do | book |
-      books << Book.new(book["title"], book["author"])
+    book_data = read_file_content('books.json')
+    book_data.each do |book|
+      books << Book.new(book['title'], book['author'])
     end
     books
   end
 
   def load_rentals(books, people)
     rentals = []
-    rental_data = read_file_content("rentals.json")
-    rental_data.each do | rental |
-      book = books.find { |item| item.title == rental["book"]}
-      person = people.find { |person| person.name == rental["person"] }
-      rentals << Rental.new(rental["date"], person, book)
+    rental_data = read_file_content('rentals.json')
+    rental_data.each do |rental|
+      book = books.find { |item| item.title == rental['book'] }
+      person = people.find { |item| item.name == rental['person'] }
+      rentals << Rental.new(rental['date'], person, book)
     end
     rentals
   end

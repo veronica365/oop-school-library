76ee159d4d2e438d5754317f213163d9399744fa
QueenterJuma <jumahqueenter@gmail.com>
2023-05-25 11:52:15 +0300

    Add student unit test

---
+++ spec/student_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../student'
+
+describe Student do
+  before :each do
+    @student = Student.new(20, 'Precious', 'true')
+  end
+
+  it 'checking student instance' do
+    expect(@student).to be_instance_of Student
+  end
+
+  it 'checking attributes' do
+    expect(@student.play_hooky).to eq('¯\(ツ)/¯')
+  end
+
+  it 'test for setter_classroom' do
+    classroom = double('classroom', students: [])
+    allow(classroom).to receive(:label) { 'Form one' }
+    @student.classroom = classroom
+    expect(@student.classroom.label).to eq('Form one')
+  end
+end
+
